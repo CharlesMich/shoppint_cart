9b25f61d5ab0bbab68a6afdda2087b1ec50a88e5
CharlesMich <charleedongo@hotmail.com>
2025-03-13 01:24:55 -0500

    Initial Commit

---
+++ OrderTaker_cupcake/OrderTaker_cupcakeApp.swift
@@ -0,0 +1,17 @@
+//
+//  OrderTaker_cupcakeApp.swift
+//  OrderTaker_cupcake
+//
+//  Created by Charles Michael on 3/13/25.
+//
+
+import SwiftUI
+
+@main
+struct OrderTaker_cupcakeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
