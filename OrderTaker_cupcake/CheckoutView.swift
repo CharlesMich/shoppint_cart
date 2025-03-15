73644eca8d893fa2e0242f184c4243da9de56c4f
CharlesMich <charleedongo@hotmail.com>
2025-03-15 10:55:09 -0500

    abc

--- OrderTaker_cupcake/CheckoutView.swift
+++ OrderTaker_cupcake/CheckoutView.swift
@@ -8,6 +8,10 @@
 import SwiftUI
 
 struct CheckoutView: View {
+    
+    @State private var confirmationMessage = ""
+    @State private var showingConfirmation = false
+    
     var order: Order
     var body: some View {
         ScrollView {
@@ -35,10 +39,26 @@ struct CheckoutView: View {
         .scrollBounceBehavior(.basedOnSize)
     }
     func placeOrder() async {
+        
         guard let encoded = try? JSONEncoder().encode(order) else {
             print("Failed to encode order")
             return
         }
+        
+        let url = URL(string: "Https://regres.in/api/cupcakes")!
+        var request = URLRequest(url: url)
+        request.setValue("application/json", forHTTPHeaderField: "content-type")
+        request.httpMethod = "POST"
+        
+        do {
+            let (data, _) = try await URLSession.shared.upload(for: request, from: encoded)
+            let decodedOrder = try JSONDecoder().decode(Order.self, from: data)
+            confirmationMessage = "Your oder for \(decodedOrder.quantity)X \(Order.types[decodedOrder.type].lowercased()) cupcakes is on its way!"
+            showingConfirmation = true
+        } catch {
+            print("Checkout failed: \(error.localizedDescription)")
+        }
+       
     }
 }
 
